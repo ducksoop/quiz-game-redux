c21c115fe8b81b6d09a2320fe5b4ed68516835f8
ducksoop <torresj1997@outlook.com>
2023-12-22 09:55:10 -0600

    feat: json lib works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
-﻿// quiz-game-redux.cpp : Defines the entry point for the application.
-//
-
+﻿#include <nlohmann/json.hpp>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	// Create a JSON object
+	nlohmann::json j;
+
+    // Add data to it
+    j["name"] = "John Doe";
+    j["age"] = 30;
+    j["is_student"] = false;
+    j["skills"] = { "C++", "Python", "JSON" };
+
+    // Serialize it to a string
+    std::string serialized = j.dump();  // You can also use dump(4) for pretty printing
+
+    // Output the serialized string
+    std::cout << serialized << std::endl;
+
+    return 0;
 }
