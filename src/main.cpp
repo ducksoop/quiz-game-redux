b104156857166c7b7e46977067a5ab72b0aa25be
ducksoop <torresj1997@outlook.com>
2023-12-22 09:39:07 -0600

    chore: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// quiz-game-redux.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
